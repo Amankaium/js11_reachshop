321bc420377c4b3b7aa774552f488831ba9afc0c
Amankaium <kaium@list.ru>
2022-06-11 21:25:41 +0600

    add theme

--- src/App.js
+++ src/App.js
@@ -12,30 +12,48 @@ import "./App.css"
 function App() {
   const [phone, setPhone] = useState("2398745928476")
   const [token, setToken] = useState("")
+  const [theme, setTheme] = useState("light")
+
+  function changeTheme() {
+    let newTheme = theme === "light" ? "dark" : "light"
+    // if (theme === "dark") {
+    //   newTheme = "light"
+    // } else {
+    //   newTheme = "dark"
+    // }
+    setTheme(newTheme)
+  }
 
   return (
     <Router>
-      <header>
-        <nav>
-          <Link to="/">Домой</Link> &nbsp;|&nbsp;&nbsp;
-          <Link to="/about">О нас</Link> &nbsp;|&nbsp;&nbsp;
-          <Link to="/contacts">Контакты</Link> &nbsp;|&nbsp;&nbsp;
-          <Link to="/orders">Заказы</Link> &nbsp;|&nbsp;&nbsp;
-          <Link to="/signup">Регистрация</Link> &nbsp;|&nbsp;&nbsp;
-          <Link to="/signin">Войти</Link>
-        </nav>
-        <span>phone: {phone}</span>
-      </header>
-      
-      <Routes>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contacts" element={<Contacts phoneNumber={phone}/>}/>
-        <Route path="/" element={<Products/>}/>
-        <Route path="/products/:id/" element={<ProductDetail/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/signin" element={<SignIn setToken={setToken}/>}/>
-        <Route path="/orders" element={<Orders token={token}/>}/>
-      </Routes>
+      <div className={theme}>
+        <header>
+          <nav>
+            <Link to="/">Домой</Link> &nbsp;|&nbsp;&nbsp;
+            <Link to="/about">О нас</Link> &nbsp;|&nbsp;&nbsp;
+            <Link to="/contacts">Контакты</Link> &nbsp;|&nbsp;&nbsp;
+            <Link to="/orders">Заказы</Link> &nbsp;|&nbsp;&nbsp;
+            <Link to="/signup">Регистрация</Link> &nbsp;|&nbsp;&nbsp;
+            <Link to="/signin">Войти</Link>
+          </nav>
+          <div>
+            <span>phone: {phone}</span>
+            <div onClick={changeTheme} className="change-theme">Сменить тему</div>
+          </div>
+        </header>
+        
+        <section className="content">
+          <Routes>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contacts" element={<Contacts phoneNumber={phone}/>}/>
+            <Route path="/" element={<Products/>}/>
+            <Route path="/products/:id/" element={<ProductDetail/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/signin" element={<SignIn setToken={setToken}/>}/>
+            <Route path="/orders" element={<Orders token={token}/>}/>
+          </Routes>
+        </section>
+      </div>
     </Router>
   );
 }
